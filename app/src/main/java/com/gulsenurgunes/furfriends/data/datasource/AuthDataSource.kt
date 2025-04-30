d4add516a089fa574ade14ffe60689cce4d55482
gulsenurgunes <gulsenurgunes@gmail.com>
2025-04-30 22:12:06 +0300

    Defined AuthDataSource interface

---
+++ app/src/main/java/com/gulsenurgunes/furfriends/data/datasource/AuthDataSource.kt
@@ -0,0 +1,7 @@
+package com.gulsenurgunes.furfriends.data.datasource
+
+import com.google.firebase.auth.FirebaseUser
+
+interface AuthDataSource {
+  suspend fun createUser(email: String, password: String): FirebaseUser
+}
