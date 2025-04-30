30a06fdbda0c3c2ef7f945a68b28b63ef1f4b327
gulsenurgunes <gulsenurgunes@gmail.com>
2025-04-30 22:13:08 +0300

    Implementing createUser via Firebase with FirebaseAuthDataSourceImpl

---
+++ app/src/main/java/com/gulsenurgunes/furfriends/data/repository/FirebaseAuthDataSourceImpl.kt
@@ -0,0 +1,32 @@
+// FirebaseAuthDataSourceImpl.kt
+package com.gulsenurgunes.furfriends.data.repository
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.gulsenurgunes.furfriends.data.datasource.AuthDataSource
+import kotlinx.coroutines.suspendCancellableCoroutine
+import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+
+class FirebaseAuthDataSourceImpl @Inject constructor(
+    private val auth: FirebaseAuth
+) : AuthDataSource {
+
+    override suspend fun createUser(
+        email: String,
+        password: String
+    ): FirebaseUser =
+        suspendCancellableCoroutine { cont ->
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener { result ->
+                    result.user?.let { cont.resume(it) }
+                        ?: cont.resumeWithException(Exception("User null"))
+                }
+                .addOnFailureListener { exc ->
+                    cont.resumeWithException(exc)
+                }
+            cont.invokeOnCancellation { }
+        }
+
+}
