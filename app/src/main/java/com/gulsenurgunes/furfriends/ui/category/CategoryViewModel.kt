7953508cade5f5d7912ddaf86fc523a1a6398786
gulsenurgunes <gulsenurgunes@gmail.com>
2025-05-11 13:37:28 +0300

    CategoryViewModel with loading/error/success handling state flow

---
+++ app/src/main/java/com/gulsenurgunes/furfriends/ui/category/CategoryViewModel.kt
@@ -0,0 +1,49 @@
+package com.gulsenurgunes.furfriends.ui.category
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gulsenurgunes.furfriends.common.Resource
+import com.gulsenurgunes.furfriends.domain.usecase.GetCategoriesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoryViewModel @Inject constructor(
+    private val getCategories: GetCategoriesUseCase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(CategoryContract.UiState())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        loadCategories()
+    }
+
+    private fun loadCategories(store: String = "FurFriends") {
+        viewModelScope.launch {
+            _uiState.update { it.copy(isLoading = true, errorMessage = null) }
+            when (val res = getCategories(store)) {
+                is Resource.Success -> _uiState.update {
+                    Log.d("CatVM", "Got categories: ${res.data.map { it.imageUrl }}")
+                    it.copy(
+                        categoryList = res.data,
+                        isLoading    = false
+                    )
+                }
+
+                is Resource.Error -> _uiState.update {
+                    it.copy(
+                        isLoading = false,
+                        errorMessage = res.message
+                    )
+                }
+            }
+        }
+    }
+
+}
