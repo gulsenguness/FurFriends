ae26aefbc6a0ab11347a78868d1052ed019696e7
gulsenurgunes <gulsenurgunes@gmail.com>
2025-05-23 00:24:19 +0300

    UIState class deleted

--- app/src/main/java/com/gulsenurgunes/furfriends/common/UIState.kt
+++
@@ -1,9 +0,0 @@
-package com.gulsenurgunes.furfriends.common
-
-import com.gulsenurgunes.furfriends.data.source.remote.model.User
-
-sealed class UIState<T> {
-    data class Loading(val isLoading: Boolean) : UIState<Any?>()
-    data class Success(val user: User) : UIState<Any?>()
-    data class Error(val message: String) : UIState<Any?>()
-}
