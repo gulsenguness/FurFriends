519f9c874197cfd7072f38f7368802b2841090e2
gulsenurgunes <gulsenurgunes@gmail.com>
2025-05-28 02:03:44 +0300

    Defined ProductDetailContract for UI state, actions, and effects

---
+++ app/src/main/java/com/gulsenurgunes/furfriends/ui/detail/ProductDetailContract.kt
@@ -0,0 +1,29 @@
+package com.gulsenurgunes.furfriends.ui.detail
+
+import com.gulsenurgunes.furfriends.data.source.remote.model.ProductDetail
+import com.gulsenurgunes.furfriends.data.source.remote.model.response.BaseResponse
+
+object ProductDetailContract {
+    data class UiState(
+        val isLoading: Boolean = false,
+        val productDetail: ProductDetail? = null,
+        val addToCart: BaseResponse? = null,
+        val errorMessage: String? = null,
+    )
+
+    sealed class UiAction {
+        data object ToggleFavoriteClick : UiAction()
+        data class AddToCartClick(val productDetail: ProductDetail) : UiAction()
+        data object BackButtonClick : UiAction()
+        data object ShareProduct : UiAction()
+    }
+
+    sealed class UiEffect {
+        data object ShowError : UiEffect()
+        data object BackScreen : UiEffect()
+        data class ShowToastMessage(val message: String) : UiEffect()
+        data object NavigateBack : UiEffect()
+        data class ShareProduct(val shareText: String) : UiEffect()
+        data class ShowAlreadyInCartMessage(val message: String) : UiEffect()
+    }
+}
