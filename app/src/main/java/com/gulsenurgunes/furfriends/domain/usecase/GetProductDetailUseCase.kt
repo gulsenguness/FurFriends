64e7ae783edfff5f6ef7e90f293463ff4bdcd422
gulsenurgunes <gulsenurgunes@gmail.com>
2025-05-28 02:03:24 +0300

    Created GetProductDetailUseCase for clean architecture separation

---
+++ app/src/main/java/com/gulsenurgunes/furfriends/domain/usecase/GetProductDetailUseCase.kt
@@ -0,0 +1,14 @@
+package com.gulsenurgunes.furfriends.domain.usecase
+
+import com.gulsenurgunes.furfriends.common.Resource
+import com.gulsenurgunes.furfriends.data.source.remote.model.ProductDetail
+import com.gulsenurgunes.furfriends.domain.repository.ProductRepository
+import javax.inject.Inject
+
+class GetProductDetailUseCase @Inject constructor(
+    private val repository: ProductRepository
+) {
+    suspend operator fun invoke(store: String, productId: Int): Resource<ProductDetail> {
+        return repository.getProductDetail(store, productId)
+    }
+}
