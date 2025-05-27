bc0b795ba0c5a0bc457cd798687113d0f9950bcc
gulsenurgunes <gulsenurgunes@gmail.com>
2025-05-28 02:03:03 +0300

    Added ProductRepositoryImpl.getProductDetail to fetch product detail from API

--- app/src/main/java/com/gulsenurgunes/furfriends/data/repository/ProductRepositoryImpl.kt
+++ app/src/main/java/com/gulsenurgunes/furfriends/data/repository/ProductRepositoryImpl.kt
@@ -2,9 +2,11 @@ package com.gulsenurgunes.furfriends.data.repository
 
 import com.gulsenurgunes.furfriends.common.Resource
 import com.gulsenurgunes.furfriends.common.mapResource
+import com.gulsenurgunes.furfriends.data.mapper.mapToProductDetail
 import com.gulsenurgunes.furfriends.data.mapper.mapToProductUi
 import com.gulsenurgunes.furfriends.data.safeApiCall
 import com.gulsenurgunes.furfriends.data.source.remote.ApiService
+import com.gulsenurgunes.furfriends.data.source.remote.model.ProductDetail
 import com.gulsenurgunes.furfriends.domain.model.ProductUi
 import com.gulsenurgunes.furfriends.domain.repository.ProductRepository
 import javax.inject.Inject
@@ -20,7 +22,9 @@ class ProductRepositoryImpl @Inject constructor(
         safeApiCall {
             api.getProductsByCategory(store, category)
         }.mapResource { dto ->
-            dto.products.mapNotNull { it.mapToProductUi() }
+            dto.products
+                .orEmpty()
+                .mapNotNull { it.mapToProductUi() }
         }
 
     override suspend fun getAllProducts(
@@ -29,7 +33,17 @@ class ProductRepositoryImpl @Inject constructor(
         safeApiCall {
             api.getProducts(store)
         }.mapResource { dto ->
-            dto.products.mapNotNull { it.mapToProductUi() }
+            dto.products
+                .orEmpty()
+                .mapNotNull { it.mapToProductUi() }
+        }
+
+    override suspend fun getProductDetail(store: String, id: Int): Resource<ProductDetail> =
+        safeApiCall {
+            api.getProductDetail(store, id)
+        }.mapResource { response ->
+            response.productDto?.mapToProductDetail(isFavorite = false)
+                ?: throw Exception("Ürün bulunamadı")
         }
 
 }
