4d9a6f28df1e8fb10bf65df96d32871c46c6a75e
gulsenurgunes <gulsenurgunes@gmail.com>
2025-05-27 15:59:22 +0300

    detail page designed

---
+++ app/src/main/java/com/gulsenurgunes/furfriends/ui/detail/DetailScreen.kt
@@ -0,0 +1,242 @@
+package com.gulsenurgunes.furfriends.ui.detail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.gulsenurgunes.furfriends.navigation.TopBar
+import com.gulsenurgunes.furfriends.ui.auth.components.AuthButton
+
+@Composable
+fun DetailScreen() {
+    Scaffold(
+        topBar = {
+            TopBar(
+                title = { Text("Product Details") },
+                navigationIcon = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {}) {
+                        Icon(Icons.Default.ShoppingCart, contentDescription = null)
+                    }
+
+                },
+            )
+        },
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .verticalScroll(rememberScrollState())
+                .padding(16.dp)
+        ) {
+            ImageSection(
+                imageUrl = "android.resource://com.gulsenurgunes.furfriends/drawable/dogs",
+                isFavorite = true,
+                onFavoriteClick = {}
+            )
+            ProductTitle(title = "Fashionable Canines: Dog Clothes For Every Season", category = "Cloth")
+
+            Spacer(Modifier.height(16.dp))
+
+            ProductPriceQuantity(price = "$270", quantity = 14)
+
+            Spacer(Modifier.height(16.dp))
+
+            SelectableItemRow("Items Size", listOf("S", "M", "L", "XL", "2XL"))
+
+            Spacer(Modifier.height(16.dp))
+
+            ColorSelectionRow()
+
+            Spacer(Modifier.height(16.dp))
+
+            Text("Description:", fontSize = 18.sp, style = MaterialTheme.typography.titleMedium)
+            Text("Fashionable and comfortable dog clothes suitable for every season.")
+
+            Spacer(Modifier.height(24.dp))
+
+            AuthButton(text = "Add To Cart", onClick = {})
+        }
+    }
+}
+
+@Composable
+fun SelectableItemRow(title: String, items: List<String>) {
+    var selectedItem by remember { mutableStateOf<String?>(null) }
+
+    Text(title, style = MaterialTheme.typography.titleMedium)
+    Row {
+        items.forEach { item ->
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .background(
+                        if (selectedItem == item) Color.Black else Color.White,
+                        CircleShape
+                    )
+                    .border(1.dp, Color.Black, CircleShape)
+                    .clickable { selectedItem = item }
+            ) {
+                Text(
+                    text = item,
+                    color = if (selectedItem == item) Color.White else Color.Black
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ColorSelectionRow(
+
+) {
+    var selectedColor by remember { mutableStateOf<Color?>(null) }
+    Text("Items Color", style = MaterialTheme.typography.titleMedium)
+    Row {
+        listOf(Color.Blue, Color.Green, Color.Magenta,Color.Red,Color.Cyan).forEach { color ->
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(end = 8.dp)
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(
+                        width = if (selectedColor == color) 2.dp else 0.dp,
+                        color = if (selectedColor == color) Color.Black else Color.Transparent,
+                        shape = CircleShape
+                    )
+                    .padding(5.dp)
+                    .background(color, CircleShape)
+                    .clickable { selectedColor = color }
+            ) {
+                if (selectedColor == color) {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = "Selected",
+                        tint = Color.White,
+                        modifier = Modifier.size(20.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+@Composable
+fun ProductTitle(title: String, category: String) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Text(category, style = MaterialTheme.typography.bodyMedium)
+        Spacer(modifier = Modifier.weight(1f))
+        Icon(Icons.Default.Star, contentDescription = "Wishlist")
+        Text("4.5", style = MaterialTheme.typography.bodyMedium)
+    }
+    Text(title, style = MaterialTheme.typography.titleMedium)
+}
+
+@Composable
+fun ProductPriceQuantity(price: String, quantity: Int) {
+    Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+        Column {
+            Text("Price", fontSize = 18.sp)
+            Text(price, fontSize = 20.sp, style = MaterialTheme.typography.titleMedium)
+        }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text("Quantity", fontSize = 18.sp)
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(Icons.Default.Warning, "Minus")
+                Text(quantity.toString())
+                Icon(Icons.Default.AddCircle, "Plus")
+            }
+        }
+    }
+}
+
+@Composable
+private fun ImageSection(
+    imageUrl: String,
+    isFavorite: Boolean,
+    onFavoriteClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .clip(RoundedCornerShape(bottomStart = 24.dp, bottomEnd = 24.dp))
+            .background(MaterialTheme.colorScheme.surface),
+        contentAlignment = Alignment.TopEnd
+    ) {
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        IconButton(
+            onClick = onFavoriteClick,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(32.dp)
+                .background(Color.White.copy(alpha = 0.8f), CircleShape)
+        ) {
+            Icon(
+                imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                contentDescription = null,
+                tint = Color.Red
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailPreview() {
+    DetailScreen()
+}
